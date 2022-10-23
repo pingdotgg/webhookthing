2047b297db96b0270fe27adaac31431fabfeaf19
Theo Browne <me@t3.gg>
2022-10-22 20:43:31 -0700

    Fix lint

--- packages/www/src/utils/trpc.ts
+++ packages/www/src/utils/trpc.ts
@@ -6,6 +6,8 @@ import { transformer } from "@captain/trpc/transformer";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
+
+  // eslint-disable-next-line
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
