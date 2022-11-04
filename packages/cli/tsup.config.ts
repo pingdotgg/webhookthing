e5ea65ffd8bcce2f4950d8064c4cd9c41b26fe27
Mark R. Florkowski <mark.florkowski@gmail.com>
2022-11-03 17:20:01 -0700

    tweaks

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -4,7 +4,6 @@ const isDev = process.env.npm_lifecycle_event === "dev";
 
 export default defineConfig({
   clean: true,
-  dts: true,
   entry: ["src/index.ts"],
   format: ["esm"],
   minify: !isDev,
