043c8f64f63d812b1fdc7869aec12815f6d5b5a4
Mark R. Florkowski <mark.florkowski@gmail.com>
2022-11-03 17:03:01 -0700

    init CLI package

---
+++ packages/cli/src/utils/renderTitle.ts
@@ -0,0 +1,18 @@
+import gradient from "gradient-string";
+import { TITLE_TEXT } from "../consts";
+
+// colors brought in from vscode poimandres theme
+const poimandresTheme = {
+  blue: "#add7ff",
+  cyan: "#89ddff",
+  green: "#5de4c7",
+  magenta: "#fae4fc",
+  red: "#d0679d",
+  yellow: "#fffac2",
+};
+
+export const renderTitle = () => {
+  const captGradient = gradient(Object.values(poimandresTheme));
+
+  console.log(captGradient.multiline(TITLE_TEXT));
+};
