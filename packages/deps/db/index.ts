0703fe563949349d62ede04164c424e85ee2e9a1
Mark R. Florkowski <mark.florkowski@gmail.com>
2022-11-05 14:20:23 -0700

    stop logging prisma queries

--- packages/deps/db/index.ts
+++ packages/deps/db/index.ts
@@ -8,10 +8,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
+    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
