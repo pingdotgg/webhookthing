b53b9212691bfa0031d4b4bfe216d405fa605173
Mark R. Florkowski <mark.florkowski@gmail.com>
2022-11-07 17:35:42 -0800

    fix hover

--- packages/www/src/pages/settings/projects/[id].tsx
+++ packages/www/src/pages/settings/projects/[id].tsx
@@ -130,7 +130,7 @@ export default function ProjectSettings() {
                   <span className="ml-3">
                     <button
                       type="button"
-                      className="disabled:hover:none inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 sm:w-auto"
+                      className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:hover:bg-indigo-600 sm:w-auto"
                       disabled={!addMemberEmail}
                       onClick={() => {
                         addMember({
