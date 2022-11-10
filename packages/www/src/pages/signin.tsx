5089a301388483c4e77888b03f4ea58467b015bc
Mark R. Florkowski <mark.florkowski@gmail.com>
2022-11-09 16:30:18 -0800

    clean up login page

--- packages/www/src/pages/signin.tsx
+++ packages/www/src/pages/signin.tsx
@@ -3,34 +3,46 @@ import type { Provider } from "next-auth/providers";
 import type { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { NoLayout } from "../components/layout/app-layout";
+import { ForwardIcon } from "@heroicons/react/24/outline";
 
 const SignIn = ({ providers }: { providers: Provider[] }) => {
   const router = useRouter();
   const callbackUrl = (router.query.redirect as string) ?? "/";
 
   return (
-    <>
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to access this page
-            </h2>
-          </div>
-          {Object.values(providers).map((provider) => (
-            <div key={provider.name}>
-              <button
-                onClick={() => signIn(provider.id, { callbackUrl })}
-                className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
-              >
-                <span className="sr-only">Sign in with {provider.name}</span>
-                <span className="capitalize">{provider.name}</span>
-              </button>
+    <div className="flex h-screen flex-col items-center justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <ForwardIcon className="mx-auto h-12 w-auto text-indigo-600" />
+            <h1 className="text-center text-3xl font-extrabold text-gray-900">
+              Captain
+            </h1>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Sign in to access this page
+              </span>
             </div>
-          ))}
+
+            <div className="mt-6 grid grid-cols-1 gap-3">
+              {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                  <button
+                    type="button"
+                    className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    onClick={() =>
+                      signIn(provider.id, { callbackUrl: callbackUrl })
+                    }
+                  >
+                    Sign In with {provider.name}
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
