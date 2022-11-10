5bf69123ab8dcf0c65567fbf31ac26d786b454b3
Mark R. Florkowski <mark.florkowski@gmail.com>
2022-11-09 16:17:05 -0800

    fix error

--- packages/www/src/components/layout/navigation.tsx
+++ packages/www/src/components/layout/navigation.tsx
@@ -17,8 +17,10 @@ export default function TopNav() {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <Link href="/">
-                    <ForwardIcon className="block h-8 w-auto text-indigo-600 lg:hidden" />
-                    <ForwardIcon className="hidden h-8 w-auto text-indigo-600 lg:block " />
+                    <>
+                      <ForwardIcon className="block h-8 w-auto text-indigo-600 lg:hidden" />
+                      <ForwardIcon className="hidden h-8 w-auto text-indigo-600 lg:block " />
+                    </>
                   </Link>
                 </div>
               </div>
