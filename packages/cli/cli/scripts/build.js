bc114d8af129007f3fe7f0a6bbb559dc6c4a3d72
Theo Browne <me@t3.gg>
2023-01-04 00:33:42 -0800

    Working build script omg

--- packages/cli/cli/scripts/build.js
+++ packages/cli/cli/scripts/build.js
@@ -1,5 +1,7 @@
-const { writeFile } = require("fs/promises");
+const { readFile, writeFile } = require("fs/promises");
 const { resolve } = require("path");
+
+const fse = require("fs-extra");
 
 const ncc = require("@vercel/ncc");
 const { join } = require("path");
@@ -8,7 +10,21 @@ const { join } = require("path");
 const DIST_DIR = resolve(__dirname, "../dist");
 const ENTRY_DIR = resolve(__dirname, "../src/index.ts");
 
+const WEB_DIR = resolve(__dirname, "../../cli-web/dist/web");
+
 async function main() {
+  try {
+    const indexHtml = await readFile(join(WEB_DIR, "index.html"), "utf8");
+  } catch (e) {
+    console.error(
+      "\n\nno index html in cli-web, make sure that is built first\n\n"
+    );
+    console.error(e);
+    process.exit(1);
+  }
+
+  fse.copySync(WEB_DIR, join(DIST_DIR, "web"));
+
   const input = "./index.js";
   const opts = { sourceMap: true, sourceMapRegister: true };
   const { code, map, assets } = await ncc(ENTRY_DIR, opts);
