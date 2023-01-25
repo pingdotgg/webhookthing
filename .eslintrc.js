c2166afc05647d2472f7b27b81379e481dfb18c0
Theo Browne <me@t3.gg>
2023-01-25 13:22:21 -0800

    FIXED ESLINT WOO (#17)

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  extends: ["prettier", "eslint:recommended"],
+  overrides: [
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["**/*.ts", "**/*.tsx"],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: [
+          "./tsconfig.json",
+          "./apps/**/*/tsconfig.json",
+          "./packages/**/*/tsconfig.json",
+        ],
+      },
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+          },
+        ],
+        "@typescript-eslint/no-floating-promises": "error",
+      },
+    },
+  ],
+  root: true,
+  reportUnusedDisableDirectives: true,
+  ignorePatterns: [
+    ".eslintrc.js",
+    "**/*.config.js",
+    "**/*.config.cjs",
+    "packages/config/**",
+  ],
+};
+
+module.exports = config;
--- packages/cli/cli-core/src/trpc.ts
+++ packages/cli/cli-core/src/trpc.ts
@@ -44,7 +44,7 @@ export const cliApiRouter = t.router({
     }),
 
   getSampleHooks: t.procedure.mutation(async () => {
-    return await getSampleHooks();
+    getSampleHooks();
   }),
 
   runFile: t.procedure
