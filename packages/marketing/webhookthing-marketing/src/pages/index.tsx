985fbc8a18304a4c32cf6af819880402134e8e26
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-01-26 12:08:07 -0800

    responsiveness tweaks

--- packages/marketing/webhookthing-marketing/src/pages/index.tsx
+++ packages/marketing/webhookthing-marketing/src/pages/index.tsx
@@ -31,10 +31,10 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-[3.5rem] font-extrabold tracking-tight text-white sm:text-[5rem]">
             webhook<span className="text-indigo-500">thing</span>
           </h1>
-          <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white">
+          <div className="flex w-96 max-w-sm flex-col gap-4 rounded-xl bg-white/10 p-6 text-white">
             <h3 className="text-2xl font-bold">join the waitlist</h3>
             <div className="flex flex-col gap-2">
               <input
@@ -63,7 +63,7 @@ const Home: NextPage = () => {
             <p className="text-center text-sm text-white/70 ">
               {showEmail ? (
                 <>
-                  {!endpoint && <>no endpoint? lame.</>}
+                  {!endpoint && !bottomText && <>no endpoint? lame.</>}
                   {bottomText}
                 </>
               ) : (
