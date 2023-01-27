05a554ce55d577d4e21cb26baad29ea6e12b87c9
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-01-27 12:49:01 -0800

    missing files

---
+++ packages/config/tailwind/index.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}", "./src/_app.tsx"],
+  theme: {
+    fontFamily: {
+      display: ["Inter"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
