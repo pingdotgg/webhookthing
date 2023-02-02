ef23502459509e2edbc869a809059280f5f61f09
Theo Browne <me@t3.gg>
2023-02-02 01:09:06 -0800

    plausible marketing

--- apps/marketing/src/pages/_app.tsx
+++ apps/marketing/src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import PlausibleProvider from "next-plausible";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="webhookthing.com">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
