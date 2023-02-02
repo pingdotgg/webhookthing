f567051cf7fcbadd99f1d0fc3398593c1e8b029c
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-02-02 01:44:02 -0800

    [W-22] Docs links (#45)
    * add docs link to cli
    
    * add docs link to web interface footer
    
    * adjust footer padding

--- apps/cli/cli/src/index.ts
+++ apps/cli/cli/src/index.ts
@@ -2,8 +2,16 @@
 import { startServer } from "./server";
 import { renderTitle } from "./utils/renderTitle";
 
+const DOCS_LINK = "https://docs.webhookthing.com";
+
 const main = async () => {
   renderTitle();
+
+  //link to docs
+  console.log(
+    `\x1b[36m%s\x1b[0m`,
+    `Questions? Check out the docs: ${DOCS_LINK}`
+  );
 
   startServer();
 };
