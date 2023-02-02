80a31973d217423a0f924e2ab73b6016b9b33ba8
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-02-02 13:54:48 -0800

    autosave endpoint onBlur

--- apps/cli/cli-web/src/components/endpointsetting.tsx
+++ apps/cli/cli-web/src/components/endpointsetting.tsx
@@ -20,7 +20,7 @@ export const EndpointSetting = () => {
           htmlFor="endpoint"
           className="block text-sm font-medium text-gray-700"
         >
-          Endpoint
+          Default Endpoint
         </label>
         <input
           type="text"
@@ -31,19 +31,8 @@ export const EndpointSetting = () => {
           onChange={(e) => {
             setEndpoint(e.target.value);
           }}
+          onBlur={() => setStoredEndpoint(endpoint)}
         />
-      </div>
-      <div className="flex flex-row items-center gap-x-2">
-        <button
-          type="button"
-          className={classNames(
-            "flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2",
-            changed ? "" : "invisible"
-          )}
-          onClick={() => setStoredEndpoint(endpoint)}
-        >
-          Save Changes
-        </button>
       </div>
     </div>
   );
