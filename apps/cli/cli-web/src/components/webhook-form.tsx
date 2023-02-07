ed15b7bfea80b4a3deacbd46bf9aa0735a6f964e
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-02-06 18:05:13 -0800

    fix borders

--- apps/cli/cli-web/src/components/webhook-form.tsx
+++ apps/cli/cli-web/src/components/webhook-form.tsx
@@ -68,7 +68,7 @@ export const WebhookForm = (input: {
       <p className="text-sm text-red-500">{errors.name?.message}</p>
       <input
         id="name"
-        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        className="block w-full rounded-md border border-gray-300 p-1 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
         {...register("name", { required: true, maxLength: 20 })}
       />
       <label htmlFor="body" className="block text-sm font-medium text-gray-700">
@@ -86,7 +86,7 @@ export const WebhookForm = (input: {
       <p className="text-sm text-red-500">{errors.config?.url?.message}</p>
       <input
         id="url"
-        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        className="block w-full rounded-md border  border-gray-300 p-1 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
         {...register("config.url")}
       />
       <fieldset>
@@ -110,7 +110,7 @@ export const WebhookForm = (input: {
                   <input
                     id={`config.headers.${index}.key`}
                     className={classNames(
-                      "relative block w-full min-w-0 flex-1 rounded-none border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
+                      "relative block w-full min-w-0 flex-1 rounded-none border border-gray-300 bg-transparent p-1 focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
                       index === 0 ? "rounded-tl-md" : ""
                     )}
                     {...register(`config.headers.${index}.key` as const)}
@@ -127,7 +127,7 @@ export const WebhookForm = (input: {
                   </label>
                   <input
                     id={`config.headers.${index}.value`}
-                    className="relative block w-auto min-w-0 flex-1 rounded-none border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    className="relative block w-full min-w-0 flex-1 rounded-none border border-gray-300 bg-transparent p-1 focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     {...register(`config.headers.${index}.value` as const)}
                     defaultValue={item.value}
                     placeholder="Value"
@@ -151,7 +151,7 @@ export const WebhookForm = (input: {
         </div>
         <button
           className={classNames(
-            "flex w-full flex-row items-center gap-1  border border-gray-300 bg-white px-4 py-2 text-start text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 ",
+            "-mt-[1px] flex w-full flex-row items-center gap-1  border border-gray-300 bg-white px-4 py-1 text-start text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 ",
             headerFields.length !== 0 ? "rounded-b-md" : "rounded-md"
           )}
           type="button"
@@ -163,12 +163,12 @@ export const WebhookForm = (input: {
       </fieldset>
       <fieldset>
         <legend className="block text-sm font-medium text-gray-700">
-          Query Params
+          Query Parameters
         </legend>
         <p className="text-sm text-red-500">{errors.config?.query?.message}</p>
         <div className="mt-1 -space-y-px rounded-md bg-white shadow-sm">
           <div className="flex flex-col -space-y-px">
-            {headerFields.map((item, index) => (
+            {queryFields.map((item, index) => (
               <div className="flex -space-x-px" key={item.id}>
                 <div className="w-1/4 min-w-0 flex-1">
                   <label
@@ -180,7 +180,7 @@ export const WebhookForm = (input: {
                   <input
                     id={`config.query.${index}.key`}
                     className={classNames(
-                      "relative block w-full min-w-0 flex-1 rounded-none border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
+                      "relative block w-full min-w-0 flex-1 rounded-none border border-gray-300 bg-transparent p-1 focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
                       index === 0 ? "rounded-tl-md" : ""
                     )}
                     {...register(`config.query.${index}.key` as const)}
@@ -197,7 +197,7 @@ export const WebhookForm = (input: {
                   </label>
                   <input
                     id={`config.query.${index}.value`}
-                    className="relative block w-auto min-w-0 flex-1 rounded-none border-gray-300 bg-transparent focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    className="relative block w-full min-w-0 flex-1 rounded-none border border-gray-300 bg-transparent p-1 focus:z-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     {...register(`config.query.${index}.value` as const)}
                     defaultValue={item.value}
                     placeholder="Value"
@@ -221,8 +221,8 @@ export const WebhookForm = (input: {
         </div>
         <button
           className={classNames(
-            "flex w-full flex-row items-center gap-1  border border-gray-300 bg-white px-4 py-2 text-start text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 ",
-            headerFields.length !== 0 ? "rounded-b-md" : "rounded-md"
+            "-mt-[1px] flex w-full flex-row items-center gap-1  border border-gray-300 bg-white px-4 py-1 text-start text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 ",
+            queryFields.length !== 0 ? "rounded-b-md" : "rounded-md"
           )}
           type="button"
           onClick={() => appendQuery({ key: "", value: "" })}
