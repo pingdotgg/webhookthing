a998a911fd4e09601098bd3be4d64418418708c7
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-02-15 17:03:44 -0800

    add ws to trpc client

--- apps/cli/cli-web/src/utils/api-wrapper.tsx
+++ apps/cli/cli-web/src/utils/api-wrapper.tsx
@@ -1,9 +1,14 @@
-import { httpBatchLink } from "@trpc/client";
+import { createWSClient, httpBatchLink, wsLink } from "@trpc/client";
 import superjson from "superjson";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, useState } from "react";
 import { cliApi } from "./api";
+
+// create persistent WebSocket connection
+const wsClient = createWSClient({
+  url: `ws://localhost:3001`,
+});
 
 export const ApiTRPCProvider = (props: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
@@ -13,6 +18,9 @@ export const ApiTRPCProvider = (props: PropsWithChildren) => {
       links: [
         httpBatchLink({
           url: "http://localhost:2033/trpc",
+        }),
+        wsLink({
+          client: wsClient,
         }),
       ],
     })
