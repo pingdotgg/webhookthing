9e4958ba022955b65e8d2d9e9fe8ef43cc029ebb
Theo Browne <me@t3.gg>
2023-02-25 16:32:24 -0800

    Add router, fix routing for static files in fastify

---
+++ apps/cli/cli-web/src/router.tsx
@@ -0,0 +1,10 @@
+import { Router, Link, Route } from "wouter";
+import AppCore from "./App";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <AppCore />
+    </Router>
+  );
+};
