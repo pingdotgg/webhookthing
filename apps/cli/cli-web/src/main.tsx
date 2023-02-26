95e3e26e5cbc9610684b6336ba5dc53947a407ce
Theo Browne <me@t3.gg>
2023-02-25 17:06:07 -0800

    Add router, fix routing for static files in fastify (#85)
    * Add router, fix routing for static files in fastify
    
    * Working router and routes, file location helper
    
    * Fix rewrite for .json and other files with . in them (gdi evan)

--- apps/cli/cli-web/src/main.tsx
+++ apps/cli/cli-web/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import { AppRouter } from "./router";
 import { ApiTRPCProvider } from "./utils/api-wrapper";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ApiTRPCProvider>
-      <App />
+      <AppRouter />
     </ApiTRPCProvider>
   </React.StrictMode>
 );
