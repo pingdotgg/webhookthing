0b271e2e5f61b1b37eddceaef87eed9c9ea0f69f
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-02-28 15:04:29 -0800

    files empty state

--- apps/cli/cli-web/src/components/filebrowser.tsx
+++ apps/cli/cli-web/src/components/filebrowser.tsx
@@ -1,4 +1,6 @@
 import {
+  CloudArrowDownIcon,
+  DocumentArrowDownIcon,
   DocumentPlusIcon,
   ExclamationCircleIcon,
   FolderIcon,
@@ -8,7 +10,10 @@ import {
   PlayIcon,
   PlusIcon,
 } from "@heroicons/react/24/solid";
-import { FolderPlusIcon as FolderPlusOutline } from "@heroicons/react/24/outline";
+import {
+  FolderPlusIcon as FolderPlusOutline,
+  DocumentPlusIcon as DocumentPlusOutline,
+} from "@heroicons/react/24/outline";
 import { Fragment, useState } from "react";
 import toast from "react-hot-toast";
 import { Link } from "wouter";
@@ -23,6 +28,7 @@ import { FolderFormModal } from "./folder-form-modal";
 import { cliApi } from "../utils/api";
 import { classNames } from "../utils/classnames";
 import { generatePrefillFromConfig } from "../utils/configTransforms";
+import { useFileRoute } from "../utils/useRoute";
 
 const pathArrToUrl = (pathArr: string[], nav?: string) => {
   const url = nav ? `${pathArr.concat(nav).join("/")}` : `${pathArr.join("/")}`;
@@ -55,9 +61,19 @@ export const FileBrowser = (input: { path: string; data: FolderDataType }) => {
     },
   });
 
+  const ctx = cliApi.useContext();
+
+  const { mutate: downloadSampleHooks } = cliApi.getSampleHooks.useMutation({
+    onSuccess: async () => {
+      await ctx.parseUrl.invalidate();
+    },
+  });
+
   const [selectedHookName, setSelectedHook] = useState<string>("");
   const addHookModalState = useState(false);
   const addFolderModalState = useState(false);
+
+  const location = useFileRoute();
 
   const selectedHook = data.files?.find((x) => x.name === selectedHookName);
 
@@ -281,45 +297,81 @@ export const FileBrowser = (input: { path: string; data: FolderDataType }) => {
           {`Files`}
         </h3>
         <div className="w-full overflow-y-auto">
-          <ul role="list" className="flex flex-col space-y-3 py-2">
-            {data.files.map((file) => (
-              <li key={file.name} className="flex w-full flex-row gap-2">
-                <Link href={pathArrToUrl(pathArr, file.name)}>
-                  <div className="group flex grow flex-row items-start justify-between gap-2 overflow-hidden rounded-md border border-gray-50 px-6 py-2 font-medium text-gray-600 shadow-sm hover:bg-indigo-100/10 hover:text-indigo-600 hover:shadow-md">
-                    {file.name}
-                    <div className="flex flex-row gap-2">
-                      {!file.config?.url ? (
-                        <Tooltip content="Missing URL" placement="left">
-                          <ExclamationCircleIcon className="h-5 w-5 text-red-600" />
-                        </Tooltip>
-                      ) : (
-                        (file.config?.headers || file.config?.query) && (
-                          <Tooltip
-                            content="This hook has a custom config"
-                            placement="left"
-                          >
-                            <InformationCircleIcon className="h-5 w-5 text-gray-600" />
-                          </Tooltip>
-                        )
-                      )}
-                    </div>
-                  </div>
-                </Link>
+          {data.files.length === 0 ? (
+            <div className="text-center">
+              <DocumentPlusOutline className="mx-auto h-12 w-12 text-gray-400" />
+              <h3 className="mt-2 text-sm font-semibold text-gray-900">
+                {`No hook files found!`}
+              </h3>
+              <p className="mt-1 text-sm text-gray-500">
+                {`Get started by `}
                 <button
-                  className="flex items-center justify-center rounded-md border border-transparent border-gray-50 px-3 text-sm font-medium leading-4 text-gray-600 shadow-sm hover:bg-indigo-100/10 hover:text-indigo-600 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 
+                  className="text-indigo-600 hover:underline"
+                  onClick={() => addHookModalState[1](true)}
+                >{`creating a new hook file`}</button>
+                {location === "/" ? (
+                  <>
+                    {`, or download some sample hooks with the button below.`}
+                    <div className="mt-6">
+                      <button
+                        type="button"
+                        className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        onClick={() => downloadSampleHooks()}
+                      >
+                        <CloudArrowDownIcon
+                          className="-ml-1 mr-2 h-5 w-5"
+                          aria-hidden="true"
+                        />
+                        {`Download Sample Hooks`}
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <>{`.`}</>
+                )}
+              </p>
+            </div>
+          ) : (
+            <ul role="list" className="flex flex-col space-y-3 py-2">
+              {data.files.map((file) => (
+                <li key={file.name} className="flex w-full flex-row gap-2">
+                  <Link href={pathArrToUrl(pathArr, file.name)}>
+                    <div className="group flex grow flex-row items-start justify-between gap-2 overflow-hidden rounded-md border border-gray-50 px-6 py-2 font-medium text-gray-600 shadow-sm hover:bg-indigo-100/10 hover:text-indigo-600 hover:shadow-md">
+                      {file.name}
+                      <div className="flex flex-row gap-2">
+                        {!file.config?.url ? (
+                          <Tooltip content="Missing URL" placement="left">
+                            <ExclamationCircleIcon className="h-5 w-5 text-red-600" />
+                          </Tooltip>
+                        ) : (
+                          (file.config?.headers || file.config?.query) && (
+                            <Tooltip
+                              content="This hook has a custom config"
+                              placement="left"
+                            >
+                              <InformationCircleIcon className="h-5 w-5 text-gray-600" />
+                            </Tooltip>
+                          )
+                        )}
+                      </div>
+                    </div>
+                  </Link>
+                  <button
+                    className="flex items-center justify-center rounded-md border border-transparent border-gray-50 px-3 text-sm font-medium leading-4 text-gray-600 shadow-sm hover:bg-indigo-100/10 hover:text-indigo-600 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 
                     disabled:cursor-not-allowed disabled:text-gray-400 disabled:hover:text-gray-400 disabled:hover:shadow-sm"
-                  onClick={() => {
-                    runFile({
-                      file: `${path}/${file.name}`,
-                    });
-                  }}
-                  disabled={!file.config?.url}
-                >
-                  <PlayIcon className="h-4" />
-                </button>
-              </li>
-            ))}
-          </ul>
+                    onClick={() => {
+                      runFile({
+                        file: `${path}/${file.name}`,
+                      });
+                    }}
+                    disabled={!file.config?.url}
+                  >
+                    <PlayIcon className="h-4" />
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
