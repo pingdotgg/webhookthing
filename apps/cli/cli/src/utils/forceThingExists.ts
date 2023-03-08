f811c24f57f900c7d71eef4944e51daff3e37803
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-03-08 13:34:51 -0800

    add logs for directory creation (#90)

--- apps/cli/cli/src/utils/forceThingExists.ts
+++ apps/cli/cli/src/utils/forceThingExists.ts
@@ -1,6 +1,8 @@
 import fs from "fs/promises";
 import path from "path";
 
+import logger from "@captain/logger";
+
 const THING_PATH = path.join(process.cwd(), ".thing");
 const HOOK_PATH = path.join(THING_PATH, "hooks");
 
@@ -8,12 +10,14 @@ export default async function forceThingExists() {
   try {
     await fs.access(THING_PATH);
   } catch (error) {
+    logger.warn("No .thing directory found, creating one now.");
     await fs.mkdir(THING_PATH);
   }
 
   try {
     await fs.access(HOOK_PATH);
   } catch (error) {
+    logger.warn("No .thing/hooks directory found, creating one now.");
     await fs.mkdir(HOOK_PATH);
   }
 }
