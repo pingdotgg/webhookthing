2addfd93397af61dc0c55b34a85f9f855e86ea2d
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-12 18:04:51 +0000

    w -> min-w to prevent children overflowing weirdly

--- apps/cli/cli-web/src/components/common/button.tsx
+++ apps/cli/cli-web/src/components/common/button.tsx
@@ -368,7 +368,7 @@ export const SplitButtonDropdownTheSequel = ({
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="min-w-36 absolute right-0 z-10 mt-2 origin-top-right  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="w-full">
               {items.map((item) => (
                 <Menu.Item key={item.name}>
