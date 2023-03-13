f25817cb625098be8907eaf52aca8b8a8662f330
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-13 19:25:09 +0000

    prevent clipping of actions with long names

--- apps/cli/cli-web/src/components/common/button.tsx
+++ apps/cli/cli-web/src/components/common/button.tsx
@@ -323,7 +323,7 @@ export const SplitButtonDropdown = ({
           leaveTo="transform opacity-0 scale-95"
         >
           <Menu.Items className="min-w-36 absolute right-0 z-10 mt-2 origin-top-right  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="w-full">
+            <div className="w-fit min-w-full">
               {items.map((item) => (
                 <Menu.Item key={item.name}>
                   {({ active }) => (
@@ -370,7 +370,7 @@ export const ButtonDropdown = ({
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="w-full">
+          <div className="w-fit min-w-full">
             {items.map((item) => (
               <Menu.Item key={item.name}>
                 {({ active }) => (
