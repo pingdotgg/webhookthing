174f44d57b24571cbf5efcdcd91941c205081323
Igor Bedesqui <igorbedesqui@gmail.com>
2023-06-01 00:15:07 +0100

    this doesn't work but I wanna save it in case my laptop explodes

--- apps/cli/cli/src/index.ts
+++ apps/cli/cli/src/index.ts
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 import { startServer, startExternalServer, startSocketServer } from "./server";
 import { renderTitle } from "./utils/renderTitle";
+import { getNpmVersion, renderVersionWarning } from "./utils/renderVersionWarning";
 
 const DOCS_LINK = "https://docs.webhookthing.com";
 
 const main = async () => {
+  const npmVersion = await getNpmVersion();
   renderTitle();
 
-  // eslint-disable-next-line no-console
-  console.log(`\x1b[36mQuestions? Check out the docs: ${DOCS_LINK}\x1b[0m`);
+  npmVersion && renderVersionWarning(npmVersion);
+  
+// eslint-disable-next-line no-console
+console.log(`\x1b[36mQuestions? Check out the docs: ${DOCS_LINK}\x1b[0m`);
 
-  startSocketServer();
+startSocketServer();
 
-  await startServer();
-  await startExternalServer();
+await startServer();
+await startExternalServer();
 };
 
 main().catch((err) => {
