607d0b09fd250011316f375c898ab6584109de63
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-13 00:45:28 +0100

    feat(cli-web): naively convert GH response into hooktree (#104)
    * feat(cli-web): naively convert GH response into hooktree
    
    * style: prettier, sorry
    
    * build(cli-web): add zod-fetch
    
    * feat(cli-web): get sample hooks from github repo

--- apps/cli/cli-web/src/components/filetree.tsx
+++ apps/cli/cli-web/src/components/filetree.tsx
@@ -1,185 +1,236 @@
-import { useEffect } from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "./common/accordion";
-import { classNames } from "../utils/classnames";
+} from './common/accordion';
+import { classNames } from '../utils/classnames';
+import { useQuery } from '@tanstack/react-query';
+import { z } from 'zod';
+import { createZodFetcher } from 'zod-fetch';
 
-// type GHResponse = {
-//   sha: string;
-//   url: string;
-//   tree: {
-//     path: string;
-//     mode: string;
-//     type: "tree" | "blob"; // don't know if this is true tho, could be a string
-//     sha: string;
-//     size?: number;
-//     url: string;
-//     tree: GHResponse["tree"];
-//   }[];
-// };
+const fetchWithZod = createZodFetcher();
+
+const GHResponseSchema = z.object({
+  sha: z.string(),
+  url: z.string(),
+  tree: z.array(
+    z.union([
+      z.object({
+        path: z.string(),
+        mode: z.string(),
+        type: z.literal('tree'),
+        sha: z.string(),
+        url: z.string(),
+      }),
+      z.object({
+        path: z.string(),
+        mode: z.string(),
+        type: z.literal('blob'),
+        sha: z.string(),
+        url: z.string(),
+        size: z.number(),
+      }),
+    ])
+  ),
+  truncated: z.boolean(),
+});
+
+type GHResponse = z.infer<typeof GHResponseSchema>;
 
 /**
  * get /hooks sha from gh api then https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/{sha}?recursive=1
  *
  * eg: https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/main?recursive=1 to get sha, then https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/5ca62d06afaa21f4dd705bacea021b02f64d61bd?recursive=1 to get data
  */
-const MOCK_SAMPLE_HOOKS_FROM_GH_API = {
-  sha: "5ca62d06afaa21f4dd705bacea021b02f64d61bd",
-  url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/5ca62d06afaa21f4dd705bacea021b02f64d61bd",
+const MOCK_SAMPLE_HOOKS_FROM_GH_API: GHResponse = {
+  sha: '5ca62d06afaa21f4dd705bacea021b02f64d61bd',
+  url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/5ca62d06afaa21f4dd705bacea021b02f64d61bd',
   tree: [
     {
-      path: "discord",
-      mode: "040000",
-      type: "tree",
-      sha: "15d4cff6a75d887b27a6813475df418079b3d448",
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/15d4cff6a75d887b27a6813475df418079b3d448",
+      path: 'discord',
+      mode: '040000',
+      type: 'tree',
+      sha: '15d4cff6a75d887b27a6813475df418079b3d448',
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/15d4cff6a75d887b27a6813475df418079b3d448',
     },
     {
-      path: "discord/discord_webhook.json",
-      mode: "100644",
-      type: "blob",
-      sha: "55c966e2c2ab1643a47af5faed674601a19481d3",
+      path: 'discord/discord_webhook.json',
+      mode: '100644',
+      type: 'blob',
+      sha: '55c966e2c2ab1643a47af5faed674601a19481d3',
       size: 960,
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/55c966e2c2ab1643a47af5faed674601a19481d3",
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/55c966e2c2ab1643a47af5faed674601a19481d3',
     },
     {
-      path: "github",
-      mode: "040000",
-      type: "tree",
-      sha: "adf9360cd7dd204b4e84e9b51bdc4e8051014a7e",
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/adf9360cd7dd204b4e84e9b51bdc4e8051014a7e",
+      path: 'github',
+      mode: '040000',
+      type: 'tree',
+      sha: 'adf9360cd7dd204b4e84e9b51bdc4e8051014a7e',
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/adf9360cd7dd204b4e84e9b51bdc4e8051014a7e',
     },
     {
-      path: "github/github_pr_opened.json",
-      mode: "100644",
-      type: "blob",
-      sha: "7a6a23c347c30474364908ccee90305bc2054a27",
+      path: 'github/github_pr_opened.json',
+      mode: '100644',
+      type: 'blob',
+      sha: '7a6a23c347c30474364908ccee90305bc2054a27',
       size: 28507,
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/7a6a23c347c30474364908ccee90305bc2054a27",
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/7a6a23c347c30474364908ccee90305bc2054a27',
     },
     {
-      path: "slack",
-      mode: "040000",
-      type: "tree",
-      sha: "614b41b234de58e2becc5e1b5f9ce6788829c2a2",
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/614b41b234de58e2becc5e1b5f9ce6788829c2a2",
+      path: 'slack',
+      mode: '040000',
+      type: 'tree',
+      sha: '614b41b234de58e2becc5e1b5f9ce6788829c2a2',
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/614b41b234de58e2becc5e1b5f9ce6788829c2a2',
     },
     {
-      path: "slack/slack_channel_message.json",
-      mode: "100644",
-      type: "blob",
-      sha: "fae6b38ba189d08e7a7daf477e5da78033961ca0",
+      path: 'slack/slack_channel_message.json',
+      mode: '100644',
+      type: 'blob',
+      sha: 'fae6b38ba189d08e7a7daf477e5da78033961ca0',
       size: 450,
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/fae6b38ba189d08e7a7daf477e5da78033961ca0",
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/fae6b38ba189d08e7a7daf477e5da78033961ca0',
     },
     {
-      path: "stripe",
-      mode: "040000",
-      type: "tree",
-      sha: "5adc1936c3c3404a0461b7d81280e5c17b2d70e1",
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/5adc1936c3c3404a0461b7d81280e5c17b2d70e1",
+      path: 'stripe',
+      mode: '040000',
+      type: 'tree',
+      sha: '5adc1936c3c3404a0461b7d81280e5c17b2d70e1',
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/5adc1936c3c3404a0461b7d81280e5c17b2d70e1',
     },
     {
-      path: "stripe/payment",
-      mode: "040000",
-      type: "tree",
-      sha: "f89c34deab0cd7c5b2d5458889af497f9363c7d0",
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/f89c34deab0cd7c5b2d5458889af497f9363c7d0",
+      path: 'stripe/payment',
+      mode: '040000',
+      type: 'tree',
+      sha: 'f89c34deab0cd7c5b2d5458889af497f9363c7d0',
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/f89c34deab0cd7c5b2d5458889af497f9363c7d0',
     },
     {
-      path: "stripe/payment/stripe_payment_failed_nested.json",
-      mode: "100644",
-      type: "blob",
-      sha: "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+      path: 'stripe/payment/stripe_payment_failed_nested.json',
+      mode: '100644',
+      type: 'blob',
+      sha: 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
       size: 0,
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
     },
     {
-      path: "stripe/payment/stripe_payment_succeeded_nested.json",
-      mode: "100644",
-      type: "blob",
-      sha: "ba0fd7c948bb0482728323084c20dc232bef7349",
+      path: 'stripe/payment/stripe_payment_succeeded_nested.json',
+      mode: '100644',
+      type: 'blob',
+      sha: 'ba0fd7c948bb0482728323084c20dc232bef7349',
       size: 6445,
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/ba0fd7c948bb0482728323084c20dc232bef7349",
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/ba0fd7c948bb0482728323084c20dc232bef7349',
     },
     {
-      path: "stripe/stripe_payment_failed.json",
-      mode: "100644",
-      type: "blob",
-      sha: "ddfff0a8767bcc92ffa96197721afce0f9402b9d",
+      path: 'stripe/stripe_payment_failed.json',
+      mode: '100644',
+      type: 'blob',
+      sha: 'ddfff0a8767bcc92ffa96197721afce0f9402b9d',
       size: 12892,
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/ddfff0a8767bcc92ffa96197721afce0f9402b9d",
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/ddfff0a8767bcc92ffa96197721afce0f9402b9d',
     },
     {
-      path: "stripe/stripe_payment_succeeded.json",
-      mode: "100644",
-      type: "blob",
-      sha: "ba0fd7c948bb0482728323084c20dc232bef7349",
+      path: 'stripe/stripe_payment_succeeded.json',
+      mode: '100644',
+      type: 'blob',
+      sha: 'ba0fd7c948bb0482728323084c20dc232bef7349',
       size: 6445,
-      url: "https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/ba0fd7c948bb0482728323084c20dc232bef7349",
+      url: 'https://api.github.com/repos/bdsqqq/sample_hooks/git/blobs/ba0fd7c948bb0482728323084c20dc232bef7349',
     },
   ],
   truncated: false,
 };
 
+const convertToTree = (response: GHResponse) => {
+  const tree = response.tree.reduce<HookTree>((acc, entry) => {
+    const { path, type } = entry;
+    const parts = path.split('/');
+    const fileName = parts[parts.length - 1];
+
+    let currentLevel = acc;
+    for (const part of parts) {
+      const existingDir = currentLevel.find((item) => item.name === part);
+      if (existingDir && 'children' in existingDir) {
+        currentLevel = existingDir.children;
+      } else {
+        const newDir: Dir = {
+          name: part,
+          children: [],
+        };
+        currentLevel.push(newDir);
+        currentLevel = newDir.children;
+      }
+    }
+
+    if (type === 'blob') {
+      const hook: Hook = {
+        name: fileName,
+      };
+      currentLevel.push(hook);
+    }
+
+    return acc;
+  }, []);
+
+  return tree;
+};
+
 const MOCK_SAMPLE_HOOKS_WiTH_JUST_NAMES = [
   {
-    name: "stripe",
-    prettyName: "Stripe",
+    name: 'stripe',
+    prettyName: 'Stripe',
 
     children: [
       {
-        name: "stripe_payment",
-        prettyName: "Payment",
+        name: 'stripe_payment',
+        prettyName: 'Payment',
         children: [
           {
-            name: "stripe_payment_succeeded_nested.json",
-            prettyName: "Payment Succeeded nested",
+            name: 'stripe_payment_succeeded_nested.json',
+            prettyName: 'Payment Succeeded nested',
           },
           {
-            name: "stripe_payment_failed_nested.json",
-            prettyName: "Payment Failed nested",
+            name: 'stripe_payment_failed_nested.json',
+            prettyName: 'Payment Failed nested',
           },
         ],
       },
 
       {
-        name: "stripe_payment_succeeded.json",
-        prettyName: "Payment Succeeded",
+        name: 'stripe_payment_succeeded.json',
+        prettyName: 'Payment Succeeded',
       },
 
       {
-        name: "stripe_payment_failed.json",
-        prettyName: "Payment Failed",
+        name: 'stripe_payment_failed.json',
+        prettyName: 'Payment Failed',
       },
     ],
   },
   {
-    name: "github",
-    prettyName: "GitHub",
+    name: 'github',
+    prettyName: 'GitHub',
 
     children: [
       {
-        name: "github_pull_request_opened.json",
-        prettyName: "Pull request opened",
+        name: 'github_pull_request_opened.json',
+        prettyName: 'Pull request opened',
       },
       {
-        name: "github_pull_request_closed.json",
-        prettyName: "Pull request closed",
+        name: 'github_pull_request_closed.json',
+        prettyName: 'Pull request closed',
       },
     ],
   },
   {
-    name: "slack",
-    prettyName: "Slack",
+    name: 'slack',
+    prettyName: 'Slack',
 
     children: [
       {
-        name: "slack_channel_message.json",
-        prettyName: "Channel message",
+        name: 'slack_channel_message.json',
+        prettyName: 'Channel message',
       },
     ],
   },
@@ -204,11 +255,11 @@ const recurseIntoAccordions = (hookTree: HookTree, nestedness = 0) => {
     safetyCounter = 0
   ): boolean => {
     if (safetyCounter > 100) {
-      throw new Error("got way too deep in a recursion");
+      throw new Error('got way too deep in a recursion');
     }
 
     return entry.children.every((child) => {
-      if ("children" in child) {
+      if ('children' in child) {
         return checkIfAllRecursiveChildrenSelected(child, safetyCounter + 1);
       }
 
@@ -222,10 +273,10 @@ const recurseIntoAccordions = (hookTree: HookTree, nestedness = 0) => {
   ): boolean => {
     return entry.children.some((child) => {
       if (safetyCounter > 100) {
-        throw new Error("got way too deep in a recursion");
+        throw new Error('got way too deep in a recursion');
       }
 
-      if ("children" in child) {
+      if ('children' in child) {
         return checkIfSomeRecursiveChildrenSelected(child, safetyCounter + 1);
       }
 
@@ -235,7 +286,7 @@ const recurseIntoAccordions = (hookTree: HookTree, nestedness = 0) => {
 
   const recursivelySelectAllChildren = (entry: Dir) => {
     entry.children.forEach((child) => {
-      if ("children" in child) {
+      if ('children' in child) {
         recursivelySelectAllChildren(child);
       } else {
         selectHook(child.name);
@@ -245,7 +296,7 @@ const recurseIntoAccordions = (hookTree: HookTree, nestedness = 0) => {
 
   const recursivelyUnselectAllChildren = (entry: Dir) => {
     entry.children.forEach((child) => {
-      if ("children" in child) {
+      if ('children' in child) {
         recursivelyUnselectAllChildren(child);
       } else {
         unselectHook(child.name);
@@ -254,7 +305,7 @@ const recurseIntoAccordions = (hookTree: HookTree, nestedness = 0) => {
   };
 
   return hookTree.map((entry) => {
-    if ("children" in entry) {
+    if ('children' in entry) {
       const areAllChildrenSelected = checkIfAllRecursiveChildrenSelected(entry);
       const areSomeChildrenSelected =
         checkIfSomeRecursiveChildrenSelected(entry);
@@ -277,12 +328,12 @@ const recurseIntoAccordions = (hookTree: HookTree, nestedness = 0) => {
                 }
               }}
               className={classNames(
-                "mr-2 inline-block h-4 w-4",
+                'mr-2 inline-block h-4 w-4',
                 areAllChildrenSelected
-                  ? "bg-green-500"
+                  ? 'bg-green-500'
                   : areSomeChildrenSelected
-                  ? "bg-orange-500"
-                  : "bg-gray-500"
+                  ? 'bg-orange-500'
+                  : 'bg-gray-500'
               )}
             />
             <AccordionTrigger>{entry.name}</AccordionTrigger>
@@ -309,8 +360,8 @@ const recurseIntoAccordions = (hookTree: HookTree, nestedness = 0) => {
               }
             }}
             className={classNames(
-              "mr-2 inline-block h-4 w-4",
-              isSelected ? "bg-green-500" : "bg-gray-500"
+              'mr-2 inline-block h-4 w-4',
+              isSelected ? 'bg-green-500' : 'bg-gray-500'
             )}
           />
           {entry.name}
@@ -326,18 +377,14 @@ export const FileTree = ({ hookTree }: { hookTree: HookTree }) => {
   );
 };
 
-import { create } from "zustand";
+import { create } from 'zustand';
 
 export const useSampleHooksStore = create<{
-  initialSampleHooks: Hook[] | undefined;
-  setInitialSampleHooks: (hooks: Hook[]) => void;
   selectedHooks: string[];
   selectHook: (hookName: string) => void;
   unselectHook: (hookName: string) => void;
   clearSelectedHooks: () => void;
 }>((set) => ({
-  initialSampleHooks: undefined,
-  setInitialSampleHooks: (hooks) => set({ initialSampleHooks: hooks }),
   selectedHooks: [],
   selectHook: (hook) =>
     set((state) => {
@@ -359,19 +406,40 @@ export const useSampleHooksStore = create<{
   clearSelectedHooks: () => set({ selectedHooks: [] }),
 }));
 
+const getHooksFromGH = async () => {
+  const repo = await fetchWithZod(
+    GHResponseSchema,
+    'https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/main'
+  );
+
+  const hooksFolderSha = repo.tree.find((entry) => entry.path === 'hooks')?.sha;
+
+  if (!hooksFolderSha) {
+    throw new Error('no hooks folder found');
+  }
+
+  const hooksFolder = await fetchWithZod(
+    GHResponseSchema,
+    `https://api.github.com/repos/bdsqqq/sample_hooks/git/trees/${hooksFolderSha}?recursive=1`
+  );
+
+  return convertToTree(hooksFolder);
+};
+
 export const TestingFileTree = () => {
-  const { initialSampleHooks, setInitialSampleHooks, selectedHooks } =
-    useSampleHooksStore();
+  const { selectedHooks } = useSampleHooksStore();
 
-  // pretend this is a TRPC query that fetches from gh;
-  useEffect(() => {
-    setInitialSampleHooks(MOCK_SAMPLE_HOOKS_WiTH_JUST_NAMES);
-  }, [setInitialSampleHooks]);
+  const { data, isLoading } = useQuery({
+    queryKey: ['hooks'],
+    queryFn: () => getHooksFromGH(),
+  });
 
-  if (!initialSampleHooks) return <div>{`loading...`}</div>;
+  if (isLoading) return <div>{`loading...`}</div>;
+  if (!data) return <div>{`no hooks found?? something went very wrong`}</div>;
+
   return (
     <>
-      <FileTree hookTree={initialSampleHooks} />
+      <FileTree hookTree={data} />
 
       <pre>{JSON.stringify(selectedHooks, null, 2)}</pre>
     </>
