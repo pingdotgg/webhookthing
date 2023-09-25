a14dbed1c191d5a8ed43d415607d58a3936d7258
Igor Bedesqui <igorbedesqui@gmail.com>
2023-09-25 21:07:26 +0100

    feat(cli-web): checkbox border and ring color

--- apps/cli/cli-web/src/components/common/checkbox.tsx
+++ apps/cli/cli-web/src/components/common/checkbox.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import { CheckIcon, MinusIcon } from '@heroicons/react/20/solid';
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { CheckIcon, MinusIcon } from "@heroicons/react/20/solid";
 
-import { classNames } from '../../utils/classnames';
+import { classNames } from "../../utils/classnames";
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
@@ -13,22 +13,22 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={classNames(
-      'border-primary focus-visible:ring-ring group peer h-4 w-4 shrink-0 rounded-sm border shadow focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:text-black',
-      className || ''
+      "group peer h-4 w-4 shrink-0 rounded border border-gray-300 shadow focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
+      className || ""
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator
-      className={classNames('flex items-center justify-center text-current')}
+      className={classNames("flex items-center justify-center text-current")}
     >
       <CheckIcon
-        height={'15px'}
-        width={'15px'}
+        height={"15px"}
+        width={"15px"}
         className="hidden h-[15px] w-[15px] group-data-[state=checked]:block"
       />
       <MinusIcon
-        height={'15px'}
-        width={'15px'}
+        height={"15px"}
+        width={"15px"}
         className="hidden h-[15px] w-[15px] group-data-[state=indeterminate]:block"
       />
     </CheckboxPrimitive.Indicator>
